a8040e4f3f58ba247fb7fbd21c63294b3ccfb483
Abbigale Bercher <abbeybercher@gmail.com>
2022-01-13 17:16:07 -0500

    react setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::API
     include ActionController::Cookies
 
   def hello_world
-    session[:session_hello] ||= "World"
-    cookies[:cookies_hello] ||= "World"
-    render json: { session: session, cookies: cookies.to_hash }
-    # session[:count] = (session[:count] || 0) + 1
-    # render json: { count: session[:count] }
+    # session[:session_hello] ||= "World"
+    # cookies[:cookies_hello] ||= "World"
+    # render json: { session: session, cookies: cookies.to_hash }
+    session[:count] = (session[:count] || 0) + 1
+    render json: { count: session[:count] }
   end
 end
