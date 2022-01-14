08b2686ec3994b5dc5aedd88538922b48b789ee9
Abbigale Bercher <abbeybercher@gmail.com>
2022-01-14 10:11:26 -0500

    Added base components signin, signup, dashboard. no interactivity

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 
-import SignupLogin from './components/SignupLogin';
-
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import Dashboard from './components/Dashboard';
 
 function App() {
 
 
   return (
     <div>
-      <SignupLogin />
+      <Dashboard />
     </div>
   );
 }
