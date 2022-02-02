3b4e62613c4122750e8a93302132f9afca192bc4
Abbigale Bercher <abbeybercher@gmail.com>
2022-02-02 12:11:58 -0500

    removed name and desc from map to reduce crowding

--- client/src/components/PinInfo.js
+++ client/src/components/PinInfo.js
@@ -6,10 +6,25 @@ import { memo } from "react";
 function PinInfo({ info }) {
   return (
     <Box sx={{ textAlign: "center" }}>
-      {info.name ? <Typography>{info.name}</Typography> : null}
-      {info.description ? <Typography>{info.description}</Typography> : null}
-      <Typography variant="body2">{info.place_name}</Typography>
-      <Typography variant="overline">
+      {/* {info.name ? <Typography variant="h6">{info.name}</Typography> : null}
+      {info.description ? (
+        <Typography
+          variant="body1"
+          // handle text overflow 
+          sx={{
+            display: "-webkit-box",
+            overflow: "hidden",
+            WebkitBoxOrient: "vertical",
+            WebkitLineClamp: 3,
+          }}
+        >
+          {info.description}
+        </Typography>
+      ) : null} */}
+      <Typography variant="h6">
+        {info.place_name}
+      </Typography>
+      <Typography variant="overline" color="#6d6d6d">
         {info.latitude}, {info.longitude}
       </Typography>
     </Box>
