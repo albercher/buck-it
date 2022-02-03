762006076d06f8e366c206a6e44e7434e98131b6
Abbigale Bercher <abbeybercher@gmail.com>
2022-02-03 13:31:06 -0500

    Changing serializer to appropriate associations

--- app/serializers/pin_serializer.rb
+++ app/serializers/pin_serializer.rb
@@ -1,5 +1,4 @@
 class PinSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :color, :place_name, :longitude, :latitude, :visited
   # has_one :user
-  has_many :activities
 end
