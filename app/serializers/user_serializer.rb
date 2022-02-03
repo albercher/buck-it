40ed808e6186d2267c46d246b0bc01132e905d70
Abbigale Bercher <abbeybercher@gmail.com>
2022-02-03 17:51:22 -0500

    Changed order of associated activities

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :initials
   has_many :pins
-  has_many :activities
+  has_many :activities do
+    object.activities.order(created_at: :desc)
+  end
 
   def initials
     (self.object.first_name[0] + self.object.last_name[0]).upcase
