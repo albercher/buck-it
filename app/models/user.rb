92c2366b6ed5ba3568c8b4f39505f1196e308af0
Abbigale Bercher <abbeybercher@gmail.com>
2022-02-11 12:12:37 -0500

    User names can now only be letters

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
     has_many :pins
     has_many :activities
     validates :email, presence: true, uniqueness: true, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/
+    validates :first_name, presence: true, format: { with: /\A[a-zA-Z]+\z/,
+        message: "only allows letters" }
+    validates :last_name, presence: true, format: { with: /\A[a-zA-Z]+\z/,
+        message: "only allows letters" }
 end
